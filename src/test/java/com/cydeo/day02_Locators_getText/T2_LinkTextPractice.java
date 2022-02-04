d0f04422b43e1f5cc7b354d2ec23c822bd239d9f
Nick <nikolayshilov@yahoo.com>
2022-02-04 12:00:52 -0500

    Selenium Practice

---
+++ src/test/java/com/cydeo/day02_Locators_getText/T2_LinkTextPractice.java
@@ -0,0 +1,52 @@
+package com.cydeo.day02_Locators_getText;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T2_LinkTextPractice {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://practice.cydeo.com");
+
+        //Click to A/B Testing from the top of the list
+        //driver.findElement(By.linkText("A/B Testing")).click();
+        WebElement abTestLink = driver.findElement(By.linkText("A/B Testing"));
+        abTestLink.click();
+
+        //Verify if title is "No A/B Test"
+        String expectedTitle = "No A/B Test";
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.equals(expectedTitle)){
+            System.out.println("Passed");
+        }else{
+            System.out.println("Failed");
+        }
+
+        //Go back to the home page using .back();
+        driver.navigate().back();
+
+        //Verify if title is "Practice"
+        expectedTitle = "Practice";
+        actualTitle = driver.getTitle();
+
+        if (actualTitle.equals(expectedTitle)){
+            System.out.println("Passed");
+        }else{
+            System.out.println("Failed");
+        }
+
+
+
+
+    }
+
+}
