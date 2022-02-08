1621b5507c5167a73ca8fd7da8d1247e609e9fef
Nick <nikolayshilov@yahoo.com>
2022-02-08 12:36:38 -0500

    utilities

--- src/test/java/com/cydeo/day03/T1_Locators_getText.java
+++ src/test/java/com/cydeo/day03/T1_Locators_getText.java
@@ -1,5 +1,6 @@
 package com.cydeo.day03;
 
+import com.cydeo.utilities.WebDriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,8 +12,11 @@ public class T1_Locators_getText {
     public static void main(String[] args) {
 
         //1. Open Chrome browser
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        //WebDriverManager.chromedriver().setup();
+        //WebDriver driver = new ChromeDriver();
+       //driver.manage().window().maximize();
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
 
         //2- Go to: https://login1.nextbasecrm.com/
