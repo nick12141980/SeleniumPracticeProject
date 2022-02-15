0d97d5ed6bfd73e8fed39c88cefe0afd50d27ad8
Nick <nikolayshilov@yahoo.com>
2022-02-15 14:53:27 -0500

    TestNG intro

--- src/test/java/com/cydeo/day05_testNG_Intro_Dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/day05_testNG_Intro_Dropdowns/TestNG_Intro.java
@@ -1,10 +1,13 @@
 package com.cydeo.day05_testNG_Intro_Dropdowns;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TestNG_Intro {
+
+    @BeforeClass
+    public void setupMethod(){
+        System.out.println("BeforeClass is running...");
+    }
 
     @BeforeMethod
     public void setUpMethod(){
