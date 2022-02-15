69fe9c6e2f95eec97376f0fbb104ff3bf8a77b6c
Nick <nikolayshilov@yahoo.com>
2022-02-15 13:07:12 -0500

    day 5 T3 Radio Buttons

--- src/test/java/com/cydeo/day05_testEng_Intro_Dropdowns/T3_RadioButton_Continue.java
+++ src/test/java/com/cydeo/day05_testEng_Intro_Dropdowns/T3_RadioButton_Continue.java
@@ -20,22 +20,24 @@ public class T3_RadioButton_Continue {
         //2. Go to https://practice.cydeo.com/radio_buttons
         driver.get("https://practice.cydeo.com/radio_buttons");
 
-        //Locate name='sports' radio buttons and store them in the list of web elements
-        List<WebElement> sportRadioButtons = driver.findElements(By.name("sport"));
-
-        //Loop through the list of the elements and select 'hockey'
-
-        for (WebElement each : sportRadioButtons) {
-            String eachID = each.getAttribute("id");
-            System.out.println("eachID = " + eachID);
-            if (eachID.equals("hockey")){
-                each.click();
-                System.out.println("Hockey is selected: "+each.isSelected());
-                break;
-            }
-        }
+       clickAndVerifyRadioButton(driver, "sport","hockey");
         driver.close();
 
 
+    }
+
+    private static void clickAndVerifyRadioButton(WebDriver driver, String nameAttribute, String idValue){
+
+        List<WebElement> radioButtons = driver.findElements(By.name(nameAttribute));
+
+        for (WebElement each : radioButtons) {
+            String eachID = each.getAttribute("id");
+
+            if (eachID.equals(idValue)){
+                each.click();
+                System.out.println(eachID +" is selected: "+each.isSelected());
+                break;
+            }
+        }
     }
 }
