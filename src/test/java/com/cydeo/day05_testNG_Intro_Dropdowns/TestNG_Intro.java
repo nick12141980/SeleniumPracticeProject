db09a194a9860f454bbd2db9badaf6b2c8074b5b
Nick <nikolayshilov@yahoo.com>
2022-02-15 14:29:21 -0500

    TestNG intro

---
+++ src/test/java/com/cydeo/day05_testNG_Intro_Dropdowns/TestNG_Intro.java
@@ -0,0 +1,12 @@
+package com.cydeo.day05_testNG_Intro_Dropdowns;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Intro {
+
+    @Test
+    public void test1(){
+        System.out.println("Test 1 is running...");
+    }
+
+}
