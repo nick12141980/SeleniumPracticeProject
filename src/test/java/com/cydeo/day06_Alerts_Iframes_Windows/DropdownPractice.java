9a231c1a83223f2a978a0787ea4eea76b1240f58
Nick <nikolayshilov@yahoo.com>
2022-02-18 11:29:13 -0500

    Dropdowns practice

---
+++ src/test/java/com/cydeo/day06_Alerts_Iframes_Windows/DropdownPractice.java
@@ -0,0 +1,45 @@
+package com.cydeo.day06_Alerts_Iframes_Windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class DropdownPractice {
+
+    @Test
+    public void dropdown_task5() throws InterruptedException {
+        //1. Open Chrome browser
+        //2. Go to http://practice.cybertekschool.com/dropdown
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://practice.cybertekschool.com/dropdown");
+
+        Select stateDropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
+        //3. Select Illinois
+        Thread.sleep(1000);
+        stateDropdown.selectByVisibleText("Illinois");
+
+        //4. Select Virginia
+        Thread.sleep(1000);
+        stateDropdown.selectByValue("VA");
+
+        //5. Select California
+        Thread.sleep(1000);
+        stateDropdown.selectByIndex(5);
+
+        //6. Verify final selected option is California.
+        String expectedOptionText = "California";
+        String actualOptionText = stateDropdown.getFirstSelectedOption().getText();
+
+        Assert.assertEquals(actualOptionText, expectedOptionText);
+
+    }
+
+
+}
