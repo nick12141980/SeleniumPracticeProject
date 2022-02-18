aede5ab8c04a3f5ecb6a5c75718a328e5d46a1f0
Nick <nikolayshilov@yahoo.com>
2022-02-18 16:52:45 -0500

    window practice

--- src/test/java/com/cydeo/day06_Alerts_Iframes_Windows/T5_WindowsPractice.java
+++ src/test/java/com/cydeo/day06_Alerts_Iframes_Windows/T5_WindowsPractice.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -56,5 +57,10 @@ public class T5_WindowsPractice {
         actualTitle = driver.getTitle();
 
         System.out.println("Title after click: "+actualTitle);
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
     }
 }
