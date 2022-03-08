e707c327addbc8579fb1664e6385dca9531cfdd4
Nick <nikolayshilov@yahoo.com>
2022-03-08 16:01:43 -0500

    Hover test

---
+++ src/test/java/com/cydeo/day10_upload_actions/T3_Actions_Hover.java
@@ -0,0 +1,55 @@
+package com.cydeo.day10_upload_actions;
+
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class T3_Actions_Hover {
+
+    @Test
+    public void hovering_test(){
+
+        //1. Go to https://practice.cydeo.com/hovers
+        Driver.getDriver().get("https://practice.cydeo.com/hovers");
+
+        //Locate all images here
+        WebElement image1 = Driver.getDriver().findElement(By.xpath("(//img)[1]"));
+        WebElement image2 = Driver.getDriver().findElement(By.xpath("(//img)[2]"));
+        WebElement image3 = Driver.getDriver().findElement(By.xpath("(//img)[3]"));
+
+        //Locate all "user" texts
+        WebElement user1 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user1']"));
+        WebElement user2 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user2']"));
+        WebElement user3 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user3']"));
+
+        Actions actions = new Actions(Driver.getDriver());
+
+        //2. Hover over to first image
+        BrowserUtils.sleep(2);
+        actions.moveToElement(image1).perform();
+        //3. Assert:
+        //a. “name: user1” is displayed
+        Assert.assertTrue(user1.isDisplayed());
+
+        //4. Hover over to second image
+        BrowserUtils.sleep(2);
+        actions.moveToElement(image2).perform();
+        //5. Assert:
+        //a. “name: user2” is displayed
+        Assert.assertTrue(user2.isDisplayed());
+
+        //6. Hover over to third image
+        BrowserUtils.sleep(2);
+        actions.moveToElement(image3).perform();
+        //7. Confirm:
+        //a. “name: user3” is displayed
+        Assert.assertTrue(user3.isDisplayed());
+
+    }
+
+
+}
