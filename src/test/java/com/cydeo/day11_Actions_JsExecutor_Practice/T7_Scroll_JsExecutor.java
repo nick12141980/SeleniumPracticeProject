118fa554a13e497819beab498fccb68b16142e29
Nick <nikolayshilov@yahoo.com>
2022-03-10 12:33:54 -0500

    JsExecutor

---
+++ src/test/java/com/cydeo/day11_Actions_JsExecutor_Practice/T7_Scroll_JsExecutor.java
@@ -0,0 +1,32 @@
+package com.cydeo.day11_Actions_JsExecutor_Practice;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class T7_Scroll_JsExecutor {
+
+    @Test
+    public void task7_Scroll_test(){
+        //1- Open a Chrome browser
+        //2- Go to: https://practice.cydeo.com/large
+        Driver.getDriver().get("https://practice.cydeo.com/large");
+
+        WebElement cydeoLink = Driver.getDriver().findElement(By.linkText("CYDEO"));
+        WebElement homeLink = Driver.getDriver().findElement(By.linkText("Home"));
+
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+
+        //3- Scroll down to “Cydeo” link
+        js.executeScript("arguments[0].scrollIntoView(true)", cydeoLink);
+
+        //4- Scroll up to “Home” link
+        js.executeScript("arguments[1].scrollIntoView(true)", cydeoLink, homeLink);
+
+        //5- Use below provided JS method only
+        //JavaScript method to use : arguments[0].scrollIntoView(true)
+    }
+
+}
