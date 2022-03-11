158c7bd1301ab04b2aa143295aeeae529a03a989
Nick <nikolayshilov@yahoo.com>
2022-03-11 16:46:36 -0500

    DynamicLoadPage1

---
+++ src/test/java/com/cydeo/pages/DynamicLoadPage1.java
@@ -0,0 +1,27 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DynamicLoadPage1 {
+
+    public DynamicLoadPage1(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//button[.='Start']")
+    public WebDriver startButton;
+
+    @FindBy(css = "div#loading")
+    public WebDriver loadingBar;
+
+    @FindBy(css = "#username")
+    public WebDriver inputUsername;
+
+    @FindBy(css = "#pwd")
+    public WebDriver inputPassword;
+
+
+}
