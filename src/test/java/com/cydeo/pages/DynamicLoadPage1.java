a6578aac7ad2aa7b46ef37af3f5363f0ca5d26b4
Nick <nikolayshilov@yahoo.com>
2022-03-11 16:51:20 -0500

    DynamicLoadPage1

--- src/test/java/com/cydeo/pages/DynamicLoadPage1.java
+++ src/test/java/com/cydeo/pages/DynamicLoadPage1.java
@@ -23,5 +23,8 @@ public class DynamicLoadPage1 {
     @FindBy(css = "#pwd")
     public WebDriver inputPassword;
 
+    @FindBy(xpath = "//button[.='Submit']")
+    public WebDriver submitButton;
+
 
 }
