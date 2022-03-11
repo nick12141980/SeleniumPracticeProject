8e0e431c3ec8ea50eccfcb3f2239b16fcbc18cd7
Nick <nikolayshilov@yahoo.com>
2022-03-11 14:40:39 -0500

    POMPractices

--- src/test/java/com/cydeo/tests/day12_POM/POMPractices.java
+++ src/test/java/com/cydeo/tests/day12_POM/POMPractices.java
@@ -1,19 +1,25 @@
 package com.cydeo.tests.day12_POM;
 
 import com.cydeo.pages.LibraryLoginPage;
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class POMPractices {
 
-    LibraryLoginPage libraryLoginPage = new LibraryLoginPage();
+    LibraryLoginPage libraryLoginPage;
+
+    @BeforeMethod
+    public void setupMethod(){
+        Driver.getDriver().get("https://library1.cydeo.com/");
+        libraryLoginPage = new LibraryLoginPage();
+
+    }
 
     @Test
     public void required_field_error_message_test(){
-        //1- Open a chrome browser
-        //2- Go to: https://library1.cydeo.com
-        Driver.getDriver().get("https://library1.cydeo.com");
 
         //3- Do not enter any information
         //4- Click to “Sign in” button
@@ -29,16 +35,13 @@ public class POMPractices {
 
     @Test
     public void invalid_email_format_test(){
-        //1- Open a chrome browser
-        //2- Go to: https://library1.cydeo.com
-        Driver.getDriver().get("https://library1.cydeo.com");
-
         //3- Enter invalid email format
         libraryLoginPage.inputUsername.sendKeys("something");
         libraryLoginPage.signInButton.click();
 
         //4- Verify expected error is displayed:
         //Expected: Please enter a valid email address.
+        BrowserUtils.sleep(1);
         Assert.assertTrue(libraryLoginPage.enterValidEmailErrorMessage.isDisplayed());
 
         Driver.closeDriver();
@@ -46,9 +49,6 @@ public class POMPractices {
 
     @Test
     public void library_negative_login_test(){
-        //1- Open a chrome browser
-        //2- Go to: https://library1.cydeo.com
-        Driver.getDriver().get("https://library1.cydeo.com");
 
         //3- Enter incorrect username or incorrect password
         libraryLoginPage.inputUsername.sendKeys("wrong@email.com");
@@ -57,6 +57,7 @@ public class POMPractices {
 
         //4- Verify title expected error is displayed:
         //Expected: Sorry, Wrong Email or Password
+        BrowserUtils.sleep(1);
         Assert.assertTrue(libraryLoginPage.wrongEmailOrPasswordErrorMessage.isDisplayed());
 
         Driver.closeDriver();
