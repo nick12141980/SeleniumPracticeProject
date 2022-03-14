b3cc7690195b11217b96bd9bd55d5af68a660d21
Nick <nikolayshilov@yahoo.com>
2022-03-14 12:26:14 -0400

    DynamicControlsPage

--- src/test/java/com/cydeo/pages/DynamicControlsPage.java
+++ src/test/java/com/cydeo/pages/DynamicControlsPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 
 public class DynamicControlsPage {
 
-    public DynamicControlsPage){
+    public DynamicControlsPage{
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
